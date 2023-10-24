d10a2f4e1fdd0fe0ea141941aeaf344051a3b718
migo1 <ertmigot@gmail.com>
2023-10-24 20:44:36 +0300

    setup-homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :categories
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "categories#index"
+  root "home#index"
   resources :categories
 end
