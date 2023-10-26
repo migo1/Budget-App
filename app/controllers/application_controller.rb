ac94eca359707d3d3c10fae82337edeaa9fb3149
migo1 <ertmigot@gmail.com>
2023-10-26 07:29:59 +0300

    add-linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
-def after_sign_in_path_for(resource)
-  if resource.is_a?(User)
-    categories_path
-  else
-    super 
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(User)
+      categories_path
+    else
+      super
+    end
   end
-end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     resource_name = User.new
-    render :index, locals: { resource_name: resource_name }
+    render :index, locals: { resource_name: }
   end
 end
